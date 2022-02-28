f6806a730f60f9663ff4183a9b1ee9aa6ead04f1
Zack <zward@academic.rrc.ca>
2022-02-28 15:10:18 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="title">
+        <h2>NetRunner Forums</h2>
+    </div>
+
+    <div class="banner">
+        <img src="computerbanner.gif">
+    </div>
+
+    <div class="text">
+        <h4 class="boards-list-font">Boards:</h4>
+    </div>
+
+    <div id="maincontainer">
+        <div class="index">
+            <div class="boardcontainer">
+                <a class="board" href="/p">/;/ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Programming</a>
+                <a class="board" href="/tech">/tech/&nbsp;&nbsp;&nbsp;&nbsp;- Technology</a>
+                <a class="board" href="/comix">/comix/ &nbsp;&nbsp;- Comics</a>
+                <a class="board" href="/mu">/mu/ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Music</a>
+                <a class="board" href="/lit">/lit/ &nbsp;&nbsp;&nbsp;&nbsp;- Literature</a>
+                <a class="board" href="/v">/v/ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Video Games</a>
+            </div>
+            
+        </div>
+    </div>
+</body>
+</html>
