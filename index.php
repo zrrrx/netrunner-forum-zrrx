3f41fd653243483900f4ce0c18ef8499858725c2
Zack <zward@academic.rrc.ca>
2022-02-28 19:32:27 -0600

    Changed project file structure

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="css\styles.css">
     <title>NetRunner - Forum</title>
 </head>
 <body>
@@ -14,7 +14,7 @@
     </div>
 
     <div class="banner">
-        <img src="computerbanner.gif">
+        <img src="assets\computerbanner.gif" alt="Markup element gif">
     </div>
 
     <div class="text">
@@ -24,12 +24,12 @@
     <div id="maincontainer">
         <div class="index">
             <div class="boardcontainer">
-                <a class="board" href="prog.php">/;/ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Programming</a>
-                <a class="board" href="tech.php">/tech/&nbsp;&nbsp;&nbsp;&nbsp;- Technology</a>
-                <a class="board" href="comix.php">/comix/ &nbsp;&nbsp;- Comics</a>
-                <a class="board" href="music.php">/mu/ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Music</a>
-                <a class="board" href="tv.php">/lit/ &nbsp;&nbsp;&nbsp;&nbsp;- Television & Film</a>
-                <a class="board" href="videogames.php">/v/ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Video Games</a>
+                <a class="board" href="src\prog.php">/;/ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Programming</a>
+                <a class="board" href="src\tech.php">/tech/&nbsp;&nbsp;&nbsp;&nbsp;- Technology</a>
+                <a class="board" href="src\comix.php">/comix/ &nbsp;&nbsp;- Comics</a>
+                <a class="board" href="src\music.php">/mu/ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Music</a>
+                <a class="board" href="src\tv.php">/tv/ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Television & Film</a>
+                <a class="board" href="src\videogames.php">/v/ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Video Games</a>
             </div>
             
         </div>
