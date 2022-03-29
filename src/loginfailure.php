d162a4e301c88f8fdeff87eb0acebfa7f2d96e56
zrrrx <deanward611@gmail.com>
2022-03-29 16:06:28 -0500

    changed styling to redirect pages

--- src/loginfailure.php
+++ src/loginfailure.php
@@ -3,12 +3,22 @@
 <head>
     <title>Redirecting...</title>
     <meta http-equiv="refresh" content="5;URL= ../index.php">
+    <link rel="stylesheet" type="text/css" href="../css/redirectstyles.css">
 </head>
 
 <body>
-    Login failed :( You are about to be redirected.<br />
-    If your browser does not redirect you in 5 seconds, or you do
-    not wish to wait, <a href="../index.php">click here</a>.
+    <p style="color: white;">
+        Login Failure :( You are about to be redirected.<br />
+        If your browser does not redirect you in 5 seconds, or you do
+        not wish to wait, <a href="../index.php">click here</a>.
+        <br>
+        <br>
+        <br>
+    </p>
+
+    <div style="text-align: center;">
+        <img src="../assets/loading.gif" width="120" height="120">
+    </div>
 </body>
 
 </html>
