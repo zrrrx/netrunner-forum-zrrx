976f5081a98775f34341bb05324b19384cdd8a8b
zrrrx <deanward611@gmail.com>
2022-03-28 20:15:41 -0500

    comment deletion

--- src/register.php
+++ src/register.php
@@ -21,27 +21,18 @@ if(isset($_POST['register'])){
     
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
     
-    //If the provided username already exists - display error.
-    //TO ADD - Your own method of handling this error. For example purposes,
-    //I'm just going to kill the script completely, as error handling is outside
-    //the scope of this tutorial.
     if($row['num'] > 0){
         die('That username already exists!');
     }
     
-    //Prepare our INSERT statement.
-    //Remember: We are inserting a new row into our users table.
     $sql = "INSERT INTO user_accounts (user_name, password) VALUES (:user_name, :password)";
     $stmt = $db->prepare($sql);
-    
-    //Bind our variables.
+
     $stmt->bindValue(':user_name', $username);
     $stmt->bindValue(':password', $pass);
 
-    //Execute the statement and insert the new account.
     $result = $stmt->execute();
-    
-    //If the signup process is successful.
+
     if($result){
         header("Location: validregister.php");
     }
