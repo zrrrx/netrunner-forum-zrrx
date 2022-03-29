394fb9830c5dac0f63370134566482459e3d3650
zrrrx <deanward611@gmail.com>
2022-03-28 22:47:02 -0500

    Added an email input to the register form. But there is an issue where the email string wont persist to database.

--- src/register.php
+++ src/register.php
@@ -11,6 +11,7 @@ if(isset($_POST['register'])){
     
     $username = !empty($_POST['user_name']) ? trim($_POST['user_name']) : null;
     $pass = !empty($_POST['password']) ? trim($_POST['password']) : null;
+    $email = !empty($_POST['email'] ? trim($_POST['email']) : null);
     
     $sql = "SELECT COUNT(user_name) AS num FROM user_accounts WHERE user_name = :user_name";
     $stmt = $db->prepare($sql);
@@ -25,11 +26,12 @@ if(isset($_POST['register'])){
         die('That username already exists!');
     }
     
-    $sql = "INSERT INTO user_accounts (user_name, password) VALUES (:user_name, :password)";
+    $sql = "INSERT INTO user_accounts (user_name, password, user_email) VALUES (:user_name, :password, :user_email)";
     $stmt = $db->prepare($sql);
 
     $stmt->bindValue(':user_name', $username);
     $stmt->bindValue(':password', $pass);
+    $stmt->bindValue(':user_email', $email);
 
     $result = $stmt->execute();
 
@@ -52,10 +54,12 @@ if(isset($_POST['register'])){
 <body>
     <h1>Register</h1>
     <form action="register.php" method="post">
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email">
         <label for="username">Username</label>
         <input type="text" id="username" name="user_name"><br>
         <label for="password">Password</label>
-        <input type="text" id="password" name="password"><br>
+        <input type="password" id="password" name="password"><br>
         <input type="submit" name="register" value="Register"></button>
     </form>
 </body>
