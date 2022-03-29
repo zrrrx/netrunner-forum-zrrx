3bb3786c715a69b3993ddcdc46c6758956d2882e
zrrrx <deanward611@gmail.com>
2022-03-29 15:02:53 -0500

    created redirects for when the user logins successfully or fails to login.

---
+++ src/loginsuccess.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Redirecting...</title>
+    <meta http-equiv="refresh" content="5;URL= ../index.php">
+</head>
+
+<body>
+    Login Success! :) You are about to be redirected.<br />
+    If your browser does not redirect you in 5 seconds, or you do
+    not wish to wait, <a href="../index.php">click here</a>.
+</body>
+
+</html>
